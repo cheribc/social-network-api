c947adab4d0e19d812e1971fa889abafdc3fca6b
Heather Cooper <90538387+cheribc@users.noreply.github.com>
2022-03-25 20:35:41 -0400

    removed sensitive info

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const { connect, connection } = require('mongoose');
 // After you create your Heroku application, visit https://dashboard.heroku.com/apps/ select the application name and add your Atlas connection string as a Config Var
 // Node will look for this environment variable and if it exists, it will use it. Otherwise, it will assume that you are running this application locally
 
-// mongodb+srv://4HCooper:24H0t.sNow@cluster0.1jm8e.mongodb.net/test
+
 
 const connectionString = 
     process.env.MONGODB_URI || 'mongodb://localhost:27017/usersDB';
